b3b1689aa17bab2ea947aef21b10df43d4ff04f5
Oneeb Asad <oneebasad1998@gmail.com>
2023-08-12 22:33:20 +0500

    Setup Alias Path Pods install Added path in babel file Added scripts in package json file Added 2 new library in package json

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      require.resolve("babel-plugin-module-resolver"),
+      {
+        cwd: "babelrc",
+        extensions: [
+          ".ios.ts",
+          ".android.ts",
+          ".ts",
+          ".ios.tsx",
+          ".android.tsx",
+          ".tsx",
+          ".jsx",
+          ".js",
+          ".json",
+        ],
+        alias: {
+          "@src": "./src",
+          "@assets": "./src/assets",
+          "@commons": "./src/commons",
+          "@configs": "./src/configs",
+          "@constants": "./src/constants",
+          "@helpers": "./src/helpers",
+          "@navigation": "./src/navigation",
+          "@core-ui": "./src/core-ui",
+        },
+      },
+    ],
+    "react-native-reanimated/plugin",
+  ],
 };
